fd1aa70572f3af1c0cd46fe018f26ee5b90682d4
Andreas Schawo <andreas@schawo.de>
2010-10-01 20:33:35 +0200

    Added shortcuts for column add/delete

--- TextTempl_de.ts
+++ TextTempl_de.ts
@@ -1,98 +1,85 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.0" language="de">
+<!DOCTYPE TS><TS version="1.1" language="de">
+<defaultcodec></defaultcodec>
 <context>
     <name>MainApp</name>
     <message>
-        <location filename="MainApp.cpp" line="82"/>
-        <location filename="MainApp.cpp" line="119"/>
-        <location filename="MainApp.cpp" line="135"/>
-        <location filename="MainApp.cpp" line="159"/>
-        <location filename="MainApp.cpp" line="207"/>
+        <location filename="MainApp.cpp" line="224"/>
         <source>Error</source>
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="77"/>
+        <location filename="MainApp.cpp" line="80"/>
         <source>Open template</source>
         <translation>Vorlage öffnen</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="77"/>
+        <location filename="MainApp.cpp" line="80"/>
         <source>Templates</source>
         <translation>Vorlagen</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="77"/>
-        <location filename="MainApp.cpp" line="130"/>
-        <location filename="MainApp.cpp" line="154"/>
-        <location filename="MainApp.cpp" line="193"/>
+        <location filename="MainApp.cpp" line="210"/>
         <source>All Files</source>
         <translation>Alle Dateien</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="82"/>
-        <location filename="MainApp.cpp" line="135"/>
+        <location filename="MainApp.cpp" line="138"/>
         <source>Couldn&apos;t open</source>
         <translation>Datei</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="119"/>
+        <location filename="MainApp.cpp" line="122"/>
         <source>The file contains no or bad template data</source>
         <translation>Die Datei enthält keine oder nicht verwertbare Vorlagedaten</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="130"/>
+        <location filename="MainApp.cpp" line="133"/>
         <source>Load data file</source>
         <translation>Datendatei laden</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="130"/>
-        <location filename="MainApp.cpp" line="154"/>
+        <location filename="MainApp.cpp" line="163"/>
         <source>CSV-File</source>
         <translation>CSV-Datei</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="154"/>
+        <location filename="MainApp.cpp" line="163"/>
         <source>Save data file</source>
         <translation>Daten speichern</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="186"/>
-        <location filename="MainApp.cpp" line="187"/>
-        <location filename="MainApp.cpp" line="243"/>
-        <location filename="MainApp.cpp" line="244"/>
+        <location filename="MainApp.cpp" line="257"/>
         <source>Name</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="193"/>
+        <location filename="MainApp.cpp" line="210"/>
         <source>Save file</source>
         <translation>Datei speichern</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="159"/>
-        <location filename="MainApp.cpp" line="207"/>
+        <location filename="MainApp.cpp" line="224"/>
         <source>Couldn&apos;t save to</source>
         <translation>Datei</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="237"/>
+        <location filename="MainApp.cpp" line="250"/>
         <source>Save all</source>
         <translation>Alle speichern</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="251"/>
+        <location filename="MainApp.cpp" line="261"/>
         <source>unnamed</source>
         <translation>Unbenannt</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="429"/>
+        <location filename="MainApp.cpp" line="451"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="429"/>
+        <location filename="MainApp.cpp" line="451"/>
         <source>TextTempl</source>
         <translation></translation>
     </message>
@@ -100,132 +87,142 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="MainApp.cpp" line="329"/>
+        <location filename="MainApp.cpp" line="355"/>
         <source>Parameter</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="MainApp.cpp" line="333"/>
+        <location filename="MainApp.cpp" line="360"/>
         <source>Value</source>
         <translation>Wert</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="85"/>
+        <location filename="TextTempl.ui" line="78"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="100"/>
+        <location filename="TextTempl.ui" line="93"/>
         <source>&amp;Help</source>
         <translation>&amp;Hilfe</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="124"/>
+        <location filename="TextTempl.ui" line="118"/>
         <source>&amp;Open</source>
         <translation>Ö&amp;ffnen</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="127"/>
+        <location filename="TextTempl.ui" line="121"/>
         <source>Ctrl+O</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="138"/>
+        <location filename="TextTempl.ui" line="133"/>
         <source>&amp;Save</source>
         <translation>&amp;Speichern</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="141"/>
+        <location filename="TextTempl.ui" line="136"/>
         <source>Ctrl+S</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="149"/>
+        <location filename="TextTempl.ui" line="145"/>
         <source>E&amp;xit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="160"/>
+        <location filename="TextTempl.ui" line="157"/>
         <source>&amp;About</source>
         <translation>&amp;Über</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="163"/>
+        <location filename="TextTempl.ui" line="160"/>
         <source>Ctrl+A</source>
         <translation>Ctrl+U</translation>
     </message>
     <message>
         <location filename="TextTempl.ui" line="202"/>
-        <source>Insert Column</source>
-        <translation>Splaten hinzufügen</translation>
+        <source>&amp;Insert Column</source>
+        <translation>Splaten h&amp;inzufügen</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="213"/>
+        <location filename="TextTempl.ui" line="205"/>
+        <source>Ctrl+I</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="TextTempl.ui" line="217"/>
         <source>Delete Column</source>
         <translation>Spalten löschen</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="224"/>
+        <location filename="TextTempl.ui" line="220"/>
+        <source>Ctrl+-</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="TextTempl.ui" line="232"/>
         <source>Load Data</source>
         <translation>Datendatei laden</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="227"/>
+        <location filename="TextTempl.ui" line="235"/>
         <source>&amp;Load Data</source>
         <translation>Datendatei &amp;laden</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="230"/>
+        <location filename="TextTempl.ui" line="238"/>
         <source>Ctrl+L</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="241"/>
+        <location filename="TextTempl.ui" line="250"/>
         <source>Close</source>
         <translation>Schließen</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="252"/>
+        <location filename="TextTempl.ui" line="262"/>
         <source>Save All</source>
         <translation>Alle speichern</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="263"/>
+        <location filename="TextTempl.ui" line="274"/>
         <source>Save Data</source>
         <translation>Daten speichern</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="152"/>
+        <location filename="TextTempl.ui" line="148"/>
         <source>Ctrl+Q</source>
         <translation>Ctrl+B</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="19"/>
+        <location filename="TextTempl.ui" line="20"/>
         <source>TextTempl</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="106"/>
+        <location filename="TextTempl.ui" line="99"/>
         <source>Edit</source>
         <translation>Bearbeiten</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="174"/>
+        <location filename="TextTempl.ui" line="172"/>
         <source>Copy</source>
         <translation>Kopieren</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="177"/>
+        <location filename="TextTempl.ui" line="175"/>
         <source>Ctrl+C</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="188"/>
+        <location filename="TextTempl.ui" line="187"/>
         <source>Paste</source>
         <translation>Einfügen</translation>
     </message>
     <message>
-        <location filename="TextTempl.ui" line="191"/>
+        <location filename="TextTempl.ui" line="190"/>
         <source>Ctrl+V</source>
         <translation></translation>
     </message>
